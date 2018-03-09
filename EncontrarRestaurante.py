50b9a28fe262d2403a31e3591b93399ee231affe
Mayke <mayke.suporte@gmail.com>
2018-03-09 15:38:36 -0400

    Implementando Encontrar Restaurante

---
+++ EncontrarRestaurante.py
@@ -0,0 +1,60 @@
+from geolocalizar import GeoLocalizar
+import json
+import httplib2
+
+import sys
+import codecs
+
+#Código padrão do python 3 para permitir acentos nas Strings
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+sys.stderr = codecs.getwriter('utf-8')(sys.stderr.detach())
+
+''' Siga os passos em https://developer.foursquare.com/docs/api/getting-started
+E cole aqui seus IDs, lembre-se, tem que criar um app, pode colocar uma url 
+ que não existe!
+'''
+foursquare_client_id = "Cole aqui seu ID Cliente"
+foursquare_client_secret = "Cole aqui seu ID SECRET"
+
+
+def acharRestaurante(tipoComida,localidade):
+	# Usando o geolocalizar para pegar latitude e longitude do Google Maps!
+    # Lembre-se que para importar o arquivo precisa estar na mesma pasta que este aqui!!!!
+	latitude, longitude = GeoLocalizar(location)
+	# Usando a API do foursquare para encontrar o restaurante mais próximo com as strings latitude, longitude e tipo de comida.
+	# Lendo a documentação, o formato da url é nesse estilo https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+    # então...
+    url = ('https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s' % (foursquare_client_id, foursquare_client_secret,latitude,longitude,mealType))
+	
+    #1. Faça você a requisição, lembre-se dos comandos padrões
+    h = # preencha aqui
+	result = # preencha aqui
+
+	print(result)
+    # Seguindo a documentação da foursquare 'venues' são os locais
+	if result['response']['venues']:
+
+		#2.  Achando o primeiro restaurante - FAÇA VOCÊ
+		restaurant = #analise a variável result e pegue o primeiro dado
+		
+        # Armazenando o nome e endereço
+		restaurant_name = restaurant['name']
+		restaurant_address = restaurant['location']['formattedAddress']
+
+        # Formatando o Endereço
+		address = ""
+		for i in restaurant_address:
+			address += i + " "
+		restaurant_address = address
+		restaurantInfo = {'name':restaurant_name, 'address':restaurant_address}
+        # Mostrando os resultados
+		print("Restaurant Name: %s" % (restaurantInfo['name']))
+		print("Restaurant Address: %s" % (restaurantInfo['address']))
+		return restaurantInfo
+	else:
+		print("No Restaurants Found for %s" % (location))
+		return "No Restaurants Found"
+
+if __name__ == '__main__':
+    #3. Rode aqui a função, mude o tipo de comida e a localização para testar
+	findARestaurant("Pizza", "porto velho, brasil")
